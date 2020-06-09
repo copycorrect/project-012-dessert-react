c3a8d8561d6e2d16986138cde3d6a97a4dfff5dc
Liliia Levda <komotoziwe@gmail.com>
2020-06-09 21:29:15 +0300

    Updates

---
+++ precache-manifest.4e1b90b81d046ec7318e2a26094ad779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90466a666e816a41766604807cd099b",
+    "url": "/project-012-dessert-react/index.html"
+  },
+  {
+    "revision": "5a6fafc1c031a70f5ab6",
+    "url": "/project-012-dessert-react/static/css/main.3f2729cb.chunk.css"
+  },
+  {
+    "revision": "32691e02bbd6ce036de8",
+    "url": "/project-012-dessert-react/static/js/2.b9d4da48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-012-dessert-react/static/js/2.b9d4da48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a6fafc1c031a70f5ab6",
+    "url": "/project-012-dessert-react/static/js/main.c3ddfb35.chunk.js"
+  },
+  {
+    "revision": "44c9a39ad943db914ffb",
+    "url": "/project-012-dessert-react/static/js/runtime-main.478bbfb4.js"
+  }
+]);
